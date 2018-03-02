236c7ba2b37730f94e85edd1c4bee3679db22766
Kamran Hetke <KHetke@gmail.com>
2018-03-02 12:04:52 -0700

    Added Text

--- src/Inlab5.java
+++ src/Inlab5.java
@@ -2,7 +2,7 @@
 public class Inlab5 {
 
 	public static void main(String[] args) {
-		System.out.println("This is a GitHub Test");
+		System.out.println("This is a GitHub Test, now updated with 100% more SPAM");
 	}
 
 }
